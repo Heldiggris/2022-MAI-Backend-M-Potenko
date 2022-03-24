c225330c7c4a7c6e91d7c167e20be7f586d200a7
Heldiggris <potenkog@gmail.com>
2022-03-24 22:56:08 +0300

    Небольшие правки LRU-cache

--- LRU-cache/cache.py
+++ LRU-cache/cache.py
@@ -1,24 +1,24 @@
 from collections import OrderedDict
  
 class LRUCache: 
-    def __init__(self, capacity: int):
+    def __init__(self, capacity: int=10):
         self.cache = OrderedDict()
         self.capacity = capacity
  
-    def get(self, key: int) -> str:
+    def get(self, key: str) -> str:
         if key not in self.cache:
             return ''
         else:
             self.cache.move_to_end(key)
             return self.cache[key]
  
-    def set(self, key: int, value: int) -> None:
+    def set(self, key: str, value: str) -> None:
         self.cache[key] = value
         self.cache.move_to_end(key)
         if len(self.cache) > self.capacity:
             self.cache.popitem(last = False)
     
-    def rem(self, key: int) -> None:
+    def rem(self, key: str) -> None:
         if key in self.cache:
             self.cache.move_to_end(key)
             self.cache.popitem()
